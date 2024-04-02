90eff48a31f06ada006a4fb5b83313a993fa62b9
merlinjaneshasirait <siraitmerlin123@gmail.com>
2024-04-02 16:50:18 +0700

    Upload File Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,29 @@
+<section>
+<head>
+<link rel="stylesheet" type="text/css" 
+href="http://localhost/pustaka-booking/assets/css/stylebuku.css">
+</head>
+
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul> <type="disc">
+        <li> Nama Depan :Merlin</li>
+        <li>Nama Belakang : Sirait</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul> <type="none">
+        <li> Jalan Parangtritis 1 No. 153 Pengasinan</li>
+    </ul>
+
+    <h4> Tempat Lahir</h4>
+    <ul> <type="none"</ul>
+        <li> Medan</li>
+    <ul>
+
+    <h4> Olah Raga Favorit</h4>
+    <ul> <type="square">
+        <li> Bulutangkis</li>
+        <li>Bola Volley</li>
+    </ul>
+</section>
